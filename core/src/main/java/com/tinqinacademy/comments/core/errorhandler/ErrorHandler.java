8e79c0a0b6be5ed01861fee3a011f7ab8f3b46a1
bogom1l <bogomil.stoev01@gmail.com>
2024-08-18 13:49:10 +0300

    Refactored ErrorHandler

--- core/src/main/java/com/tinqinacademy/comments/core/errorhandler/ErrorHandler.java
+++ core/src/main/java/com/tinqinacademy/comments/core/errorhandler/ErrorHandler.java
@@ -1,11 +1,9 @@
 package com.tinqinacademy.comments.core.errorhandler;
 
-
 import com.tinqinacademy.comments.api.error.Error;
 import com.tinqinacademy.comments.api.error.ErrorsWrapper;
 import com.tinqinacademy.comments.api.exceptions.CommentException;
 import com.tinqinacademy.comments.api.exceptions.ValidationException;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,49 +16,51 @@ import static io.vavr.Predicates.instanceOf;
 
 @Component
 public class ErrorHandler {
-
     public ErrorsWrapper handleErrors(Throwable throwable) {
-        List<Error> errors = new ArrayList<>();
-
-        HttpStatus status = Match(throwable).of(
-                Case($(instanceOf(MethodArgumentNotValidException.class)), ex -> handleMethodArgumentNotValidException(ex, errors)),
-                Case($(instanceOf(ValidationException.class)), ex -> handleValidationException(ex, errors)),
-                Case($(instanceOf(CommentException.class)), ex -> handleCommentException(ex, errors)),
-                Case($(), ex -> handleGenericException(ex, errors))
+        return Match(throwable).of(
+                Case($(instanceOf(MethodArgumentNotValidException.class)), ex -> handleMethodArgumentNotValidException(ex)),
+                Case($(instanceOf(ValidationException.class)), ex -> handleValidationException(ex)),
+                Case($(instanceOf(CommentException.class)), ex -> handleCommentException(ex)),
+                Case($(), ex -> handleGenericException(ex))
         );
+    }
 
+    private static Error createError(String field, String message) {
+        return Error.builder()
+                .field(field)
+                .message(message)
+                .build();
+    }
+
+    private static ErrorsWrapper createErrorsWrapper(List<Error> errors, HttpStatus status) {
         return ErrorsWrapper.builder()
                 .errors(errors)
                 .httpStatus(status)
                 .build();
     }
 
-    private HttpStatus handleValidationException(ValidationException ex, List<Error> errors) {
-        ex.getViolations().forEach(violation -> errors.add(Error.builder()
-                .field(violation.getField())
-                .message(violation.getMessage())
-                .build()));
-        return HttpStatus.BAD_REQUEST;
+    private static ErrorsWrapper handleValidationException(ValidationException ex) {
+        List<Error> errors = new ArrayList<>();
+        ex.getViolations().forEach(violation -> errors.add(createError(violation.getField(), violation.getMessage())));
 
+        return createErrorsWrapper(errors, HttpStatus.BAD_REQUEST);
     }
 
-    private HttpStatus handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, List<Error> errors) {
+    private static ErrorsWrapper handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<Error> errors = new ArrayList<>();
         ex.getBindingResult().getFieldErrors()
-                .forEach(error ->
-                        errors.add(Error.builder()
-                                .field(error.getField())
-                                .message(error.getDefaultMessage())
-                                .build()));
-        return HttpStatus.BAD_REQUEST;
+                .forEach(error -> errors.add(createError(error.getField(), error.getDefaultMessage())));
+
+        return createErrorsWrapper(errors, HttpStatus.BAD_REQUEST);
     }
 
-    private HttpStatus handleCommentException(CommentException ex, List<Error> errors) {
-        errors.add(Error.builder().message(ex.getMessage()).build());
-        return HttpStatus.NOT_FOUND;
+    private static ErrorsWrapper handleCommentException(CommentException ex) {
+        List<Error> errors = List.of(createError(null, ex.getMessage()));
+        return createErrorsWrapper(errors, HttpStatus.NOT_FOUND);
     }
 
-    private HttpStatus handleGenericException(Throwable ex, List<Error> errors) {
-        errors.add(Error.builder().message(ex.getMessage()).build());
-        return HttpStatus.INTERNAL_SERVER_ERROR;
+    private static ErrorsWrapper handleGenericException(Throwable ex) {
+        List<Error> errors = List.of(createError(null, ex.getMessage()));
+        return createErrorsWrapper(errors, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
