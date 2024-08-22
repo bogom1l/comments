1869f6970a6dcc7e0d8a88877f9f0fb9d96ff862
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-22 13:14:22 +0300

    Cleaned up

--- restexport/src/main/java/com/tinqinacademy/restexport/CommentsRestExportClient.java
+++ restexport/src/main/java/com/tinqinacademy/restexport/CommentsRestExportClient.java
@@ -11,15 +11,11 @@ import com.tinqinacademy.comments.api.operations.getcomments.GetCommentsOutput;
 import com.tinqinacademy.comments.api.restroutes.RestApiRoutes;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@FeignClient(name = "comments", url = "http://localhost:8080")
 @FeignClient(name = "comments")
 @Headers("Content-Type: application/json")
 public interface CommentsRestExportClient {
-
-    //todo - change the return type to the correct output model
 
     @GetMapping(RestApiRoutes.GET_ALL_COMMENTS)
     GetCommentsOutput getAllComments(@PathVariable String roomId);
